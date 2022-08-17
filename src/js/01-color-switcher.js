234559eec43134686f0eb40497700dfc8494aa5d
Bohdan Pishchak <3.14shcha@gmail.com>
2022-08-17 13:38:13 +0300

    T-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+const btnStartRef = document.querySelector('[data-start]');
+const btnStoptRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body')
+
+btnStartRef.addEventListener('click', startColorSwitch);
+btnStoptRef.addEventListener('click', stopColorSwitch);
+
+let startId = null;
+btnStoptRef.setAttribute('disabled', 'disabled');
+
+function startColorSwitch() {
+    startId = setInterval(() => {
+    changeColor();
+    }, 1000);
+    btnStartRef.setAttribute('disabled', 'disabled');
+    btnStoptRef.removeAttribute('disabled');
+}
+
+function stopColorSwitch() {
+    clearInterval(startId);
+    btnStartRef.removeAttribute('disabled');
+    btnStoptRef.setAttribute('disabled', 'disabled');
+}
+
+function changeColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
